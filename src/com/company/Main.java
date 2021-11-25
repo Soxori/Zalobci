349264a3acfe6173adbc11dc4d66cfe5828e4091
Jakub Panek <jakub.panek@educanet.cz>
2021-11-25 14:37:01 +0100

    csvAdded

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
