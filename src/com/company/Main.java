e2f624ba0a8ee72bf2d61157e676a992dbc924f2
Jakub Panek <jakub.panek@educanet.cz>
2021-11-25 15:34:45 +0100

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,17 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) throws FileNotFoundException {
+        File input = new File("D:\\Zalobci\\src\\com\\company\\naklady_zastoupeni.csv");
+        Scanner sc = new Scanner(input);
+        while(sc.hasNextLine()) {
+            String[] soudce = sc.nextLine().split(",");
+            System.out.println();
+        }
     }
 }
